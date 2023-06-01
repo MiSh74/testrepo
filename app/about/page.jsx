8781be929ba29cce87875c760c2ff0055c4bc52c
webdeveloper510 <webdeveloper510@gmail.com>
2023-06-01 16:11:53 +0530

    hello

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>About
+        <Link href="/test" replace></Link>
+    </div>
   )
 }
 
--- pages/test.jsx
+++ pages/test.jsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Test = () => {
   return (
-    <div>Test</div>
+    <div>Test
+        <Link href="/about" replace></Link>
+    </div>
   )
 }
 
